73507eed2f5b72697eed58dd46b095413813e030
EmmanuelMess <emmanuelbendavid@gmail.com>
2020-11-17 23:53:03 -0300

    Move to deep Q learning

--- main.py
+++ main.py
@@ -2,74 +2,84 @@ import numpy
 import pygame as game
 from rimworldenv import SimpleRimWorldEnv
 
+import gym
+import collections
+import random
+
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
-from torch.distributions import Categorical
 
 # Hyperparameters
-learning_rate = 0.0002
+learning_rate = 0.0005
 gamma = 0.98
-n_rollout = 10
+buffer_limit = 50000
+batch_size = 32
 
 
-class ActorCritic(nn.Module):
-    def __init__(self, sizeGrid: int, maxActors: int):
-        super(ActorCritic, self).__init__()
-        self.data = []
+class ReplayBuffer():
+    def __init__(self):
+        self.buffer = collections.deque(maxlen=buffer_limit)
 
-        self.fc1 = nn.Linear(sizeGrid, 256)
-        self.fc_pi = nn.Linear(256, sizeGrid*maxActors + sizeGrid*maxActors)
-        self.fc_v = nn.Linear(256, 1)
-        self.optimizer = optim.Adam(self.parameters(), lr=learning_rate)
+    def put(self, transition):
+        self.buffer.append(transition)
 
-    def pi(self, x, softmax_dim=0):
-        x = F.relu(self.fc1(x))
-        x = self.fc_pi(x)
-        prob = F.softmax(x, dim=softmax_dim)
-        return prob
+    def sample(self, n):
+        mini_batch = random.sample(self.buffer, n)
+        s_lst, a_lst, r_lst, s_prime_lst, done_mask_lst = [], [], [], [], []
 
-    def v(self, x):
-        x = F.relu(self.fc1(x))
-        v = self.fc_v(x)
-        return v
-
-    def put_data(self, transition):
-        self.data.append(transition)
-
-    def make_batch(self):
-        s_lst, a_lst, r_lst, s_prime_lst, done_lst = [], [], [], [], []
-        for transition in self.data:
-            s, a, r, s_prime, done = transition
+        for transition in mini_batch:
+            s, a, r, s_prime, done_mask = transition
             s_lst.append(s)
             a_lst.append([a])
-            r_lst.append([r / 100.0])
+            r_lst.append([r])
             s_prime_lst.append(s_prime)
-            done_mask = 0.0 if done else 1.0
-            done_lst.append([done_mask])
+            done_mask_lst.append([done_mask])
 
-        s_batch, a_batch, r_batch, s_prime_batch, done_batch = torch.tensor(s_lst, dtype=torch.float), torch.tensor(
-            a_lst), \
-                                                               torch.tensor(r_lst, dtype=torch.float), torch.tensor(
-            s_prime_lst, dtype=torch.float), \
-                                                               torch.tensor(done_lst, dtype=torch.float)
-        self.data = []
-        return s_batch, a_batch, r_batch, s_prime_batch, done_batch
+        return torch.tensor(s_lst, dtype=torch.float), torch.tensor(a_lst), \
+               torch.tensor(r_lst), torch.tensor(s_prime_lst, dtype=torch.float), \
+               torch.tensor(done_mask_lst)
 
-    def train_net(self):
-        s, a, r, s_prime, done = self.make_batch()
-        td_target = r + gamma * self.v(s_prime) * done
-        delta = td_target - self.v(s)
+    def size(self):
+        return len(self.buffer)
 
-        pi = self.pi(s, softmax_dim=1)
-        pi_a = pi.gather(1, a)
-        loss = -torch.log(pi_a) * delta.detach() + F.smooth_l1_loss(self.v(s), td_target.detach())
 
-        self.optimizer.zero_grad()
-        loss.mean().backward()
-        self.optimizer.step()
+class Qnet(nn.Module):
+    def __init__(self, sizeGrid, maxActors):
+        super(Qnet, self).__init__()
+        self.fc1 = nn.Linear(sizeGrid, 128)
+        self.fc2 = nn.Linear(128, 128)
+        self.fc3 = nn.Linear(128, sizeGrid*maxActors + sizeGrid*maxActors)
 
+    def forward(self, x):
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
+
+    def sample_action(self, obs, epsilon):
+        out = self.forward(obs)
+        coin = random.random()
+        if coin < epsilon:
+            return random.randint(0, 1)
+        else:
+            return out.argmax().item()
+
+
+def train(q, q_target, memory, optimizer):
+    for i in range(10):
+        s, a, r, s_prime, done_mask = memory.sample(batch_size)
+
+        q_out = q(s)
+        q_a = q_out.gather(1, a)
+        max_q_prime = q_target(s_prime).max(1)[0].unsqueeze(1)
+        target = r + gamma * max_q_prime * done_mask
+        loss = F.smooth_l1_loss(q_a, target)
+
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
 
 def actionMove(targetId: int, pos: tuple):
     return targetId, (True, pos), 0, (False, (0, 0))
@@ -95,32 +105,39 @@ def translateToAction(result: int):
 def main():
     screen = game.display.set_mode((640, 480))
     env = SimpleRimWorldEnv(SIZE_X, SIZE_Y, screen)
-    model = ActorCritic(SIZE_X*SIZE_Y, MAX_ACTORS)
+    q = Qnet(SIZE_X*SIZE_Y, MAX_ACTORS)
+    q_target = Qnet(SIZE_X*SIZE_Y, MAX_ACTORS)
+    q_target.load_state_dict(q.state_dict())
+    memory = ReplayBuffer()
+
     print_interval = 20
     score = 0.0
+    optimizer = optim.Adam(q.parameters(), lr=learning_rate)
 
-    for n_epi in range(100000):
-        done = False
+    for n_epi in range(10000):
+        epsilon = max(0.01, 0.08 - 0.01 * (n_epi / 200))  # Linear annealing from 8% to 1%
         s = env.reset()
+        done = False
+
         while not done:
-            for t in range(n_rollout):
-                prob = model.pi(torch.from_numpy(s).float())
-                m = Categorical(prob)
-                a = m.sample().item()
-                s_prime, r, done, info = env.step(translateToAction(a))
-                model.put_data((s, a, r, s_prime, done))
+            a = q.sample_action(torch.from_numpy(s).float(), epsilon)
+            s_prime, r, done, info = env.step(translateToAction(a))
+            done_mask = 0.0 if done else 1.0
+            memory.put((s, a, r / 100.0, s_prime, done_mask))
+            s = s_prime
 
-                s = s_prime
-                score += r
+            score += r
+            env.render()
+            if done:
+                break
 
-                env.render()
-                if done:
-                    break
-
-            model.train_net()
+        if memory.size() > 2000:
+            train(q, q_target, memory, optimizer)
 
         if n_epi % print_interval == 0 and n_epi != 0:
-            print("# of episode :{}, avg score : {:.1f}".format(n_epi, score / print_interval))
+            q_target.load_state_dict(q.state_dict())
+            print("n_episode :{}, score : {:.1f}, n_buffer : {}, eps : {:.1f}%".format(
+                n_epi, score / print_interval, memory.size(), epsilon * 100))
             score = 0.0
     env.close()
 
